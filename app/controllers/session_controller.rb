baad6feac619bf915d36ce2b3d5c8b6e3a638bd5
Vivian Wan <vivianpswan@gmail.com>
2020-04-04 15:58:36 +1100

    added login backend

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,24 @@
+class SessionController < ApplicationController
+  
+  # def new 
+  # end
+
+  def create
+    user = User.find_by :name => params[:name]
+    # user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      # session[:user_id] = user.id # Log the user in
+      # redirect_to root_path
+      render json {user: user }
+    else
+      # flash[:error] = "Invalid email address or password"
+      # redirect_to login_path
+      render json: {errors: "Invalid username"}
+    end
+  end
+
+  # def destroy
+  #   session[:user_id] = nil # Log the user out
+  #   redirect_to login_path
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  root :to => 'pages#welcome'
+  resources :users, :only => [:new, :create, :index]
+  
+  get 'users/new'
   get 'records/index'
   get 'records/new'
   get 'records/edit'
   get 'records/show'
+
+  # get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  # delete '/login' => 'session#destroy'
+
 resources :records
 end
