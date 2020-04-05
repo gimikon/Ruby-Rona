280b1754c35bd9bc7f9a20e0e68d2eba847cb0b7
Vivian Wan <vivianpswan@gmail.com>
2020-04-06 00:52:34 +1000

    still fixing login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,50 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    if @users
+      render json: {
+        users: @users
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['no users found']
+      }
+    end
   end
 
   def new
     @user = User.new
   end
 
+  # def create
+  #   @user = User.new user_params
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def create
-    @user = User.new user_params
+    @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render :new
+      login!
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else 
+      render json: {
+        status: 500,
+        errors: @user.errors.full_messages
+      }
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
